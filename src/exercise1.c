0a36a14a59ca88d21ff3ea02b7f6f520cc752036
Петухов Артём Эдуардович ИП-512 <artempetuhov123@yandex.ru>
2025-10-06 20:05:53 +0700

    1

--- src/exercise1.c
+++ src/exercise1.c
@@ -3,7 +3,6 @@ int main(int argc, char** argv) {
     float array[10];
     float sum =0;
     float res = 0;
-    printf("Введите десять   чисел:\n");
     for(int i = 0; i < 10; ++i) {
         scanf("%f", &array[i]);
         sum += array[i];
