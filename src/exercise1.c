0a30efc2dfbc6defc8f7a9d77b96e2254fb9404b
Петухов Артём Эдуардович ИП-512 <artempetuhov123@yandex.ru>
2025-10-06 23:49:30 +0700

    1.8

--- src/exercise1.c
+++ src/exercise1.c
@@ -3,12 +3,10 @@
 int main() {
     double array[10];
     double sum =0;
-    double res = 0;
     for(int i = 0; i < 10; ++i) {
         scanf("%lf", &array[i]);
         sum += array[i];
     }
-    res = (double)sum / 10;
-    
-    printf("%.2lf\n", res);
+    double qw = round(sum * 100) / 1000; 
+    printf("%.2lf\n", qw);
 }
